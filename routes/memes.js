f2771b4f79e62802c4950611c1334ab777c9904a
Richard Houet <itchy.ooc@gmail.com>
2019-05-04 17:18:18 +0200

    send 10 + pass 10 for tops

--- routes/memes.js
+++ routes/memes.js
@@ -51,13 +51,13 @@ router.get('/:id/bestofalltime', authHelper.checkAuth, function(req,res,next){
   var next_page = req.query.next;
     if (next_page && next_page > 1) {
       req.db.collection.find({type: "MEME_TYPE"}
-      ).sort({upvote: -1 , downvote: 1}).skip(next_page - 1).limit(5).toArray(function(err, docs) {
+      ).sort({upvote: -1 , downvote: 1}).skip(next_page * 10).limit(10).toArray(function(err, docs) {
         if (err) return next(err);
         res.status(200).json(docs)
       })
     } else {
       req.db.collection.find({type: "MEME_TYPE"}
-      ).sort({upvote: -1 , downvote: 1}).limit(5).toArray(function(err, docs) {
+      ).sort({upvote: -1 , downvote: 1}).limit(10).toArray(function(err, docs) {
         if (err) return next(err);
         res.status(200).json(docs)
     })
